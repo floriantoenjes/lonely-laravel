44cbf4287b5c764b44485bed3a9c40ba3c9b3352
Florian Tönjes <skatepainter@yahoo.de>
2020-10-24 16:28:09 +0200

    Cleaned up

--- routes/web.php
+++ routes/web.php
@@ -39,23 +39,18 @@ Route::middleware(['auth:sanctum', 'verified'])->post('/company', function (\Ill
 
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/lonely-dashboard', function () {
-    $userLonelySettings = Auth::user()->userLonelySetting()->first();
+    $currentUserLonelySettings = Auth::user()->userLonelySetting()->first();
 
     $lonely = false;
-    if ($userLonelySettings !== null) {
-        $userLonelySinceDateTime = new DateTime($userLonelySettings->lonely_since);
-        $userLonelySinceDateTime->setTime(0, 0);
-
-        $now = new DateTime();
-        $now->setTime(0, 0);
-
-        $lonely = ($now->getTimestamp() === $userLonelySinceDateTime->getTimestamp());
+    if ($currentUserLonelySettings !== null) {
+        $lonely = isLonelyToday($currentUserLonelySettings);
     }
 
     $lonelyPersonSettings = UserLonelySetting::where(DB::raw('DATE(lonely_since)'), '=', DB::raw('CURDATE()'))->get();
+
     $lonelyPersonIds = [];
     foreach ($lonelyPersonSettings as $lonelyPersonSetting) {
-        if ($lonelyPersonSetting->user_id === $userLonelySettings->user_id) {
+        if ($lonelyPersonSetting->user_id === $currentUserLonelySettings->user_id) {
             continue;
         }
         $lonelyPersonIds[] = $lonelyPersonSetting->user_id;
@@ -64,41 +59,45 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/lonely-dashboard', functi
     $lonelyPersons = User::whereIn('id', $lonelyPersonIds)->get();
 
     return Inertia\Inertia::render('LonelyDashboard', [
-        'userLonelySettings' => $userLonelySettings,
+        'userLonelySettings' => $currentUserLonelySettings,
         'success' => true,
         'lonely' => $lonely,
         'lonelyPersons' => $lonelyPersons
     ]);
 })->name('lonely-dashboard');
 
+
+/**
+ * @param $userLonelySettings
+ * @return bool
+ * @throws Exception
+ */
+function isLonelyToday($userLonelySettings): bool
+{
+    $userLonelySinceDateTime = new DateTime($userLonelySettings->lonely_since);
+    $userLonelySinceDateTime->setTime(0, 0);
+
+    $now = new DateTime();
+    $now->setTime(0, 0);
+
+    $lonely = ($now->getTimestamp() === $userLonelySinceDateTime->getTimestamp());
+    return $lonely;
+}
+
+
 Route::middleware(['auth:sanctum', 'verified'])->post('/lonely-dashboard', function (\Illuminate\Http\Request $request) {
     $address = $request->input('address') . ' ' . $request->input('city') . ' ' . $request->input('postcode');
+
     $coordinates = getCoordinatesFromAddress($address);
     if ($coordinates === false) {
         return Redirect::route('lonely-dashboard');
     }
 
-    $lonelySetting = Auth::user()->userLonelySetting()->firstOr(['*'], function () {
-        return new UserLonelySetting();
-    });
-
-    $lonelySetting->latitude = $coordinates['latitude'];
-    $lonelySetting->longitude = $coordinates['longitude'];
-    $lonelySetting->city = $request->input('city');
-    $lonelySetting->postcode = $request->input('postcode');
-    $lonelySetting->address = $request->input('address');
-
-    $lonelySetting->lonely_since = date('c');
-
-    $lonelySetting->radius = $request->input('radius');
-    $lonelySetting->meet_up_age_from = $request->input('ageFrom');
-    $lonelySetting->meet_up_age_to = $request->input('ageTo');
-
-    $lonelySetting->user_id = Auth::user()->id;
-    $lonelySetting->save();
+    createOrUpdateLonelySetting($coordinates, $request);
 
     return Redirect::route('lonely-dashboard');
 });
+
 
 function getCoordinatesFromAddress(string $address) {
 //    $prepAddr = str_replace(' ','+',$address);
@@ -118,4 +117,31 @@ function getCoordinatesFromAddress(string $address) {
         'latitude' => '53.044941',
         'longitude' => '8.517674'
     ];
+}
+
+
+/**
+ * @param array $coordinates
+ * @param \Illuminate\Http\Request $request
+ */
+function createOrUpdateLonelySetting(array $coordinates, \Illuminate\Http\Request $request): void
+{
+    $lonelySetting = Auth::user()->userLonelySetting()->firstOr(['*'], function () {
+        return new UserLonelySetting();
+    });
+
+    $lonelySetting->latitude = $coordinates['latitude'];
+    $lonelySetting->longitude = $coordinates['longitude'];
+    $lonelySetting->city = $request->input('city');
+    $lonelySetting->postcode = $request->input('postcode');
+    $lonelySetting->address = $request->input('address');
+
+    $lonelySetting->lonely_since = date('c');
+
+    $lonelySetting->radius = $request->input('radius');
+    $lonelySetting->meet_up_age_from = $request->input('ageFrom');
+    $lonelySetting->meet_up_age_to = $request->input('ageTo');
+
+    $lonelySetting->user_id = Auth::user()->id;
+    $lonelySetting->save();
 }
