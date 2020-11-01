37c4b5e374dea3f81acea4332fc5b231b71ddbf6
Florian Tönjes <skatepainter@yahoo.de>
2020-11-01 19:36:56 +0100

    Added create activity post route and controller method

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Activity;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class ActivityController extends Controller
@@ -23,8 +24,24 @@ class ActivityController extends Controller
 
     public function createActivity(Request $request)
     {
+        // TODO: Extract function from DashboardController
+        $coordinates = [
+            'latitude' => '53.044941',
+            'longitude' => '8.517674'
+        ];
+
         $activity = new Activity();
-        // name, description, city, postcode, address
+        $activity->name = $request->input('name');
+        $activity->description = $request->input('description');
+        $activity->city = $request->input('city');
+        $activity->postcode = $request->input('postcode');
+        $activity->address = $request->input('address');
+
+        $activity->latitude = $coordinates[0];
+        $activity->longitude = $coordinates[1];
+
+        $activity->creator_id = Auth::user()->id;
+        $activity->save();
     }
 
 }
--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -5,6 +5,38 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Activity
+ *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $name
+ * @property string $description
+ * @property string $latitude
+ * @property string $longitude
+ * @property string $city
+ * @property int $postcode
+ * @property string $address
+ * @property string $date
+ * @property int $creator_id
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCreatorId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereLatitude($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereLongitude($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity wherePostcode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Activity extends Model
 {
     use HasFactory;
